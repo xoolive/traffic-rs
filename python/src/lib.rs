8b7d1dfbe1e3181bf5bdf9d0cba97b217c26ecb1
Xavier Olive <git@xoolive.org>
2025-11-15 14:38:13 +0100

    comment failing architectures, fix import issue

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -1,3 +1,5 @@
+//! Python bindings for thrust core functionalities.
+
 use pyo3::prelude::*;
 
 pub mod intervals;
@@ -7,9 +9,15 @@ pub mod kalman;
 #[pymodule]
 #[pyo3(name = "core")]
 fn thrust(py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
-    // Add submodules
     let interval_mod = intervals::init(py)?;
     m.add_submodule(&interval_mod)?;
+
+    // This works "from thrust.core import intervals" in Python
+    // The following allows to import as "import thrust.core.intervals"
+    // or "from thrust.core.intervals import ..."
+    let sys = py.import("sys")?;
+    let modules = sys.getattr("modules")?;
+    modules.set_item("thrust.core.intervals", &interval_mod)?;
 
     #[cfg(any(feature = "openblas", feature = "netlib"))]
     {
